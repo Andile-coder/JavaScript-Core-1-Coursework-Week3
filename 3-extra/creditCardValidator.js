7b47f395c35a58c8dc8df3c2dfc7c451a38dd295
Andile-coder <61992639+Andile-coder@users.noreply.github.com>
2021-01-29 12:40:28 +0200

    Update creditCardValidator
    working on the credit card validator used more callback functions to replace block functions.

--- 3-extra/creditCardValidator.js
+++ 3-extra/creditCardValidator.js
@@ -19,23 +19,17 @@ function getLastDigit(cardNumber) {
   );
 }
 let cardNumber = "1111111111111111";
-console.log(cardNumber.length);
 let cardNumbertoArray = cardNumber.split(""); //forms an array of strings
 let makeNumber = cardNumbertoArray.map((Arrayitem) => Number(Arrayitem)); // converts array elements to numbers
-
-function addDigits(arr) {
-  //calculates the sum of all the digits
-  let sum = 0;
-  for (let i = 1; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return "CardNumber summation: " + (sum > 16);
-}
-function checkDuplicate(arr) {
-  return arr === makeNumber[0];
-}
-console.log(addDigits(makeNumber));
 console.log(getLastDigit(makeNumber));
 console.log(numberLength(makeNumber));
 console.log(noString(cardNumber));
 console.log(makeNumber.every(checkDuplicate));
+
+function cardNumberValidation(cardNumber) {
+  let cardNumbertoArray = cardNumber.split(""); //forms an array of strings
+  let makeNumber = cardNumbertoArray.map((Arrayitem) => Number(Arrayitem)); // converts array elements to numbers
+  let sum = makeNumber.reduce((a, b) => a + b); // sums all the value of the array
+  let dublicate = makeNumber.every((num) => num === makeNumber[0]); // checks if there are different numbers in the card number
+}
+console.log(cardNumberValidation("1111111111111111"));
